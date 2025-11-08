e081e76d5f9bf17bcce47cc8e70be4b10752034f
SofiaFasii <sofiafasii97@gmail.com>
2025-11-08 18:37:45 +0200

    верстка

--- js/rock-paper-scissors.js
+++ js/rock-paper-scissors.js
@@ -7,8 +7,11 @@ const rpsResult = document.querySelector('.rps-result')
 const playerChoice = (choice) => rpsPlayerSelection = choice; 
 
 function playRound(){
+    rpsResult.classList.remove('draw', 'success', 'failure');
+    
     if(!rpsPlayerSelection){
-        rpsResult.textContent = 'Спочатку оберіть варіант';
+        rpsResult.textContent = 'Спочатку оберіть свій хід!';
+        rpsResult.classList.add('failure');
         return;
     }
 
@@ -17,15 +20,20 @@ function playRound(){
 
     if(rpsPlayerSelection === compSelection){
         rpsResult.textContent = 'Нічія';
+        rpsResult.classList.add('draw') ;
     }
     else if(rpsPlayerSelection === 'ножиці' && compSelection === 'папір' || rpsPlayerSelection === 'папір' && compSelection === 'камінь' || rpsPlayerSelection === 'камінь' && compSelection === 'ножиці'){
         rpsResult.textContent = 'Ви виграли раунд!';
+        rpsResult.classList.add('success');
         rpsPlayerScore++;
     }
     else{
         rpsResult.textContent = 'Комп’ютер виграв раунд!';
+        rpsResult.classList.add('failure');
         rpsCompScore++;
     }
-    document.querySelector('.rps-score-computer').textContent = `Комп’ютер - ${rpsPlayerScore}`
-    document.querySelector('.rps-score-user').textContent = `Ви - ${rpsCompScore}`
+    document.querySelector('.rps-score-computer').textContent = `Комп’ютер - ${rpsCompScore}`
+    document.querySelector('.rps-score-user').textContent = `Ви - ${rpsPlayerScore}`
+
+    rpsPlayerSelection = '';
 }
