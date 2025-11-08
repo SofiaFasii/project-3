664264099d4bda9e296485d7df0dc77ad9bf3045
artemtanasiichuk <aizensama2356@gmail.com>
2025-11-08 17:17:35 +0100

    da

---
+++ js/number.js
@@ -0,0 +1,19 @@
+const inputs = document.querySelectorAll('input');
+const output = document.getElementById('max');
+
+inputs.forEach(input => {
+  input.addEventListener('input', () => {
+    const values = Array.from(inputs)
+      .map(i => Number(i.value))
+      .filter(v => !isNaN(v) && i.value !== '');
+
+    if (values.length === 0) {
+      output.textContent = "-";
+      return;
+    }
+
+    const maxValue = Math.max(...values);
+    output.textContent = maxValue;
+  });
+});
+
