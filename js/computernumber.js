d24233b17736331f2ed892a8a1506a16b8a70a81
artemtanasiichuk <aizensama2356@gmail.com>
2025-11-19 16:17:25 +0100

    Update computernumber.js

--- js/computernumber.js
+++ js/computernumber.js
@@ -1,4 +1,4 @@
-let randomNumber = Math.floor(Math.random() * 100) + 1;
+let randomNumber = Math.floor(Math.random() * 10) + 1;
 const input = document.getElementById("guess");
 const btn = document.getElementById("check-btn");
 const message = document.getElementById("message");
@@ -7,7 +7,7 @@ btn.addEventListener("click", () => {
     const userGuess = Number(input.value);
 
     if (!userGuess ||  userGuess < 1 ||  userGuess > 100) {
-    message.textContent = "Введіть число від 1 до 100!";
+    message.textContent = "Введіть число від 1 до 10!";
     message.style.color = "#990000";
 } else if (userGuess === randomNumber) {
     message.textContent = `Вітаю, ви вгадали число!(${ randomNumber })`;
